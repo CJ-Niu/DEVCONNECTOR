408b94c9f7d193e4b4bc93076b76b8c21b87c0b7
CJ Liu <wonder.xichen@gmail.com>
2023-01-19 23:11:45 -0600

    fix

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -165,7 +165,7 @@ router.get('/user/:user_id', async (req, res) => {
 // @access              Private
 router.delete('/', auth, async (req, res) => {
     try {
-        // @todo - remove users postss
+        // @todo - remove users posts
 
         // Remove profile
         await Profile.findOneAndRemove({ user: req.user.id });
