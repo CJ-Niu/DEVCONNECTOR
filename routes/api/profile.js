8bb9edab68982ee782dc06f030d11a476a9a8f58
CJ Liu <wonder.xichen@gmail.com>
2023-01-23 22:37:54 -0600

    work!

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -360,7 +360,7 @@ router.put(
     router.get('/github/:username', (req, res) => {
         try {
             const options = {
-                uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc$client_id=${cofig.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+                uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc$client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
 
                 method: 'GET',
                 headers: { 'user-agent': 'node.js' }
