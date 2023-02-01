76fa4b2d4be077832f1e86658a23b321e41a2525
CJ Liu <wonder.xichen@gmail.com>
2023-01-31 22:38:05 -0600

    action setup

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -19,5 +19,15 @@ export default function (state = initialState, action) {
         isAuthenticated: true,
         loading: false,
       };
+    case REGISTER_FAIL:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+      };
+    default:
+      return state;
   }
 }
