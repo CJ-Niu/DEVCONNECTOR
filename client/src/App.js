53417260fa75e0976a369f2ee92ac15ec2573507
CJ Liu <wonder.xichen@gmail.com>
2023-02-01 23:22:53 -0600

    useEffect hooks

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,32 @@ import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
 
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Navbar />
-      <Alert />
-      <Routes>
-        <Route exact path='/' element={<Landing />} />
-        <Route exact path='/register' element={<Register />} />
-        <Route exact path='/login' element={<Login />} />
-      </Routes>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token); // set header with token, if token exist
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser);
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
