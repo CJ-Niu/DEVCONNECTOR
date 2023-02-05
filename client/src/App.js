2dbe903d199d495ffc31134ba57d932363adee98
CJ Liu <wonder.xichen@gmail.com>
2023-02-04 23:04:08 -0600

    dashboard private route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -32,6 +34,7 @@ const App = () => {
           <Route exact path='/' element={<Landing />} />
           <Route exact path='/register' element={<Register />} />
           <Route exact path='/login' element={<Login />} />
+          <Route exact path='/dashboard' element={<Dashboard />} />
         </Routes>
       </Router>
     </Provider>
