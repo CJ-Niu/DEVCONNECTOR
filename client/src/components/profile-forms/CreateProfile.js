4245921da4a3cc028b3e6ca1194dcac4515f8140
CJ Liu <wonder.xichen@gmail.com>
2023-02-09 23:28:56 -0600

    start profile forms

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CreateProfile = (props) => {
+  const [formData, setFormData] = useState({
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: '',
+  });
+
+  return <div>CreateProfile</div>;
+};
+
+CreateProfile.propTypes = {};
+
+export default CreateProfile;
