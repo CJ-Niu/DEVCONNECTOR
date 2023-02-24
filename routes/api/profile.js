f961ee4512532a79ab499927059ce11923ccbadb
CJ Liu <wonder.xichen@gmail.com>
2023-02-23 23:11:56 -0600

    bug fixed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const config = require('config');
 
-const router = express.Router(); // express router
+const router = express.Router();
 const auth = require('../../middleware/auth');
 
 const { check, validationResult } = require('express-validator/check');
