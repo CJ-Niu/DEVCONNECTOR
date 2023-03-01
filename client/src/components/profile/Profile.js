2d08d6cc4c2b150907b6413a3dc0ab47b5e6beb9
CJ Liu <wonder.xichen@gmail.com>
2023-02-28 22:42:40 -0600

    starting profile about

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
